f07a26d508ae52eec97bd63f3a277a8890208ab8
Joseph Wong <joseph.wong@d2l.com>
2020-06-11 11:54:52 -0400

    ran setup

--- test/attendance.test.js
+++ test/attendance.test.js
@@ -1,19 +1,19 @@
-import '../<%= shortName %>.js';
+import '../attendance.js';
 import { expect, fixture, html } from '@open-wc/testing';
 import { runConstructor } from '@brightspace-ui/core/tools/constructor-test-helper.js';
 
-describe('<%= name %>', () => {
+describe('d2l-labs-attendance', () => {
 
 	describe('accessibility', () => {
 		it('should pass all axe tests', async() => {
-			const el = await fixture(html`<<%= name %>></<%= name %>>`);
+			const el = await fixture(html`<d2l-labs-attendance></d2l-labs-attendance>`);
 			await expect(el).to.be.accessible();
 		});
 	});
 
 	describe('constructor', () => {
 		it('should construct', () => {
-			runConstructor('<%= name %>');
+			runConstructor('d2l-labs-attendance');
 		});
 	});
 
